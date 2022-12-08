972651422e5e1d78d4d6ca4db84cde215a2abe84
Sadoubencherif <sadoubencherif98@gmail.com>
2022-12-08 17:40:46 +0000

    Ci ajouter

--- src/main/java/org/example/Wallet.java
+++ src/main/java/org/example/Wallet.java
@@ -1,14 +1,1 @@
-package org.example;
 
-public class Wallet {
-    private  double solde;
-
-
-    public double getSolde() {
-        return solde;
-    }
-
-    public void setSolde(double solde) {
-        this.solde = solde;
-    }
-}
